ae38172080fb33fd64962300d766ce7f9a867ecc
2DAW04 <2DAW04@esvirgua.es>
2022-01-18 11:52:25 +0100

    Arreglado el bug de cargar()

--- src/js/vista.js
+++ src/js/vista.js
@@ -22,8 +22,6 @@ export class Vista {
     this.dirHTML = Vista.dirHTML
     this.dirCSS = Vista.dirCSS
 
-    this.promesas = [];
-
     this.html = {} //Referencias a Elementos HTML en la plantilla
     this.hijos = {} //Referencias a Subvistas
   }
@@ -36,33 +34,31 @@ export class Vista {
   */
   cargar(plantilla, base = this.base) {
     //nombre por defecto
-    if (!plantilla) plantilla = `${this.dirHTML}/${this.constructor.name.toLowerCase()}.html`; //Nombre del fichero en minusculas.
-    return new Promise((resolve, reject) => { fetch(plantilla)
-      .then(respuesta => {
-        respuesta.text().then(texto => {
-            const parser = new DOMParser()
-            let doc = parser.parseFromString(texto, "text/html")
-            this.registrar(doc)
-            this.asociar()
-            this.transferir(base, doc.body)	//De lo contrario, cargamos todo el doc (con html, head, body...)
-            this.cargarCSS(`${this.dirCSS}/${this.constructor.name.toLowerCase()}.css`)
-            this.crearHijos() //Carga todos los hijos de la vista principal
-            for (let hijo in this.hijos) {
-              this.promesas.push(this.hijos[hijo].cargar());
-              //console.log(hijo);
-            }
-            console.log(this.promesas);
-            resolve(this.promesas);
-            //Promise.all(this.promesas)//.then(e=> console.log(e));
+    if (!plantilla) plantilla = `${this.dirHTML}/${this.constructor.name.toLowerCase()}.html`
+    return new Promise(resolve => {
+      fetch(plantilla)
+        .then(respuesta => {
+          respuesta.text().then(texto => {
+              const parser = new DOMParser()
+              let doc = parser.parseFromString(texto, "text/html")
+              this.registrar(doc)
+              this.asociar()
+              this.transferir(base, doc.body)	//De lo contrario, cargamos todo el doc (con html, head, body...)
+              this.cargarCSS(`${this.dirCSS}/${this.constructor.name.toLowerCase()}.css`)
+              this.crearHijos()
+              const promesas = [] //Creamos un array de promesas
+              for (let hijo in this.hijos)
+                promesas.push(this.hijos[hijo].cargar())
+              return Promise.all(promesas).then(() => {
+                console.log(`Cargado ${this.constructor.name.toLowerCase()}`)
+                resolve(true)
+              })
           })
-      })
+        })
       .catch(error => {
-        reject(error);
-        //throw error
+        throw error
       })
-    });
-
-      //return Promise.all(this.promesas);
+    })
   }
   /**
   	Carga un fichero de CSS en la cabecera del documento.
